5b5472d314dc9505bf9064b9328a7a4d7a3d0bae
rezzaSyahputra <rezza.sr@gmail.com>
2024-12-07 15:11:51 +0700

    menambahkan logger

--- src/main/java/com/mentos74/catalogue/web/BookController.java
+++ src/main/java/com/mentos74/catalogue/web/BookController.java
@@ -3,10 +3,13 @@ package com.mentos74.catalogue.web;
 import com.mentos74.catalogue.dto.BookDetailDTO;
 import com.mentos74.catalogue.services.BookService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.StopWatch;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @AllArgsConstructor
 @RestController
 public class BookController {
@@ -15,7 +18,13 @@ public class BookController {
 
     @GetMapping("/book/{bookId}")
     public BookDetailDTO findBookDetail(@PathVariable("bookId") Long id) {
-        return bookService.findBookDetailById(id);
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        log.info("Start find detail");
+        BookDetailDTO result = bookService.findBookDetailById(id);
+        log.info("End find detail, execution time = "+ stopWatch.getTotalTimeMillis());
+
+        return result;
     }
 }
 
