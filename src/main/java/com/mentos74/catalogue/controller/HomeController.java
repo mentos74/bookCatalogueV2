7d366296b8e99fe4dc25730833a0008802d9f81e
mentos74 <rezza.sr@gmail.com>
2024-12-08 07:34:20 +0700

    add thymeleave, dan testing home

---
+++ src/main/java/com/mentos74/catalogue/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.mentos74.catalogue.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+    @GetMapping(value = "/home")
+//@RequestMapping(value = "/home", method = RequestMethod.GET)
+    public String home(Model model) {
+        return "home";
+    }
+
+}
